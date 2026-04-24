7ef93dcfb7cabef924cd2032d5415271f6c71712
Alex Xu <xalex@mit.edu>
2026-04-23 22:46:03 -0400

    test: add stories for axis title

--- packages/gofish-graphics/stories/forwardsyntax/Bar/BarAxesPermutations.stories.tsx
+++ packages/gofish-graphics/stories/forwardsyntax/Bar/BarAxesPermutations.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from "@storybook/html";
 import { initializeContainer } from "../../helper";
 import { seafood } from "../../../src/data/catch";
 import { Chart, spread, rect } from "../../../src/lib";
+import type { AxesOptions } from "../../../src/ast/gofish";
 
 const meta: Meta = {
   title: "Forward Syntax V3/Bar/Axes Permutations",
@@ -17,10 +18,9 @@ const meta: Meta = {
 
 export default meta;
 
-type AxesOption = boolean | { x: boolean; y: boolean };
 type Args = { w: number; h: number };
 
-function renderBar(args: Args, axes: AxesOption): HTMLElement {
+function renderBar(args: Args, axes: AxesOptions): HTMLElement {
   const container = initializeContainer();
 
   Chart(seafood)
@@ -63,4 +63,36 @@ export const AxesYOnly: StoryObj<Args> = {
 export const AxesXYFalse: StoryObj<Args> = {
   args: { w: 400, h: 400 },
   render: (args: Args) => renderBar(args, { x: false, y: false }),
+};
+
+// y is undefined, only x axis shown
+export const AxesXOnlyUndefinedY: StoryObj<Args> = {
+  args: { w: 400, h: 400 },
+  render: (args: Args) => renderBar(args, { x: true }),
+};
+
+// x is undefined, only y axis shown
+export const AxesYOnlyUndefinedX: StoryObj<Args> = {
+  args: { w: 400, h: 400 },
+  render: (args: Args) => renderBar(args, { y: true }),
+};
+
+// inferred titles from field encodings
+export const AxesInferredTitles: StoryObj<Args> = {
+  args: { w: 400, h: 400 },
+  render: (args: Args) => renderBar(args, true),
+};
+
+// explicit title override on x, inferred on y
+export const AxesCustomXTitle: StoryObj<Args> = {
+  args: { w: 400, h: 400 },
+  render: (args: Args) =>
+    renderBar(args, { x: { title: "Lake" }, y: true }),
+};
+
+// title: false suppresses the inferred title
+export const AxesSuppressedTitle: StoryObj<Args> = {
+  args: { w: 400, h: 400 },
+  render: (args: Args) =>
+    renderBar(args, { x: { title: false }, y: true }),
 };
